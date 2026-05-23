e152a0a8c7b48f396856a3fa8f164feee8f16bf1
Alpaca Communist <alpaca.communist@gmail.com>
2026-05-23 19:03:45 +0300

    adds ckHookOnMapEnter

--- src/map.cc
+++ src/map.cc
@@ -1,3 +1,5 @@
+#include "ck_scripting.h"
+
 #include "map.h"
 
 #include <assert.h>
@@ -1071,6 +1073,15 @@ err:
         scriptSoundStop(mapLoadSoundId);
         mapLoadSoundId = 0;
     }
+
+
+    // Possibly this?
+    // if (rc == 0) {
+    //     ckHookOnMapEnter();
+    // }
+
+    // But for now just this
+    ckHookOnMapEnter();
 
     return rc;
 }
